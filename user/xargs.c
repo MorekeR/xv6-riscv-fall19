fb440089ea6eed1ca4d7b559f10c09b2aeb7199e
MorekeR <799178829@qq.com>
2020-11-22 15:40:34 +0800

    lab1

--- user/xargs.c
+++ user/xargs.c
@@ -2,59 +2,63 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
-void main(int argc, char *argv[]) {
-  int i, j, pid, len;
-  int first_blank;
-  char args[MAXARG][32];
-  char *p[MAXARG];
-  char buf;
+void main(int argc, char *argv[])
+{
+    int i, j, pid, len;
+    int first_blank;
+    char args[MAXARG][32];
+    char *p[MAXARG];
+    char buf;
 
-  if(argc < 2) {
-    fprintf(2, "usage: xargs <cmd> ...\n");
-    exit();
-  }
-  while (1)
-  {
-    // init
-    i = 0;
-    first_blank = 0;
-    memset(args, 0, MAXARG * 32);
-    // push command args and stdin args together
-    // the first arg must be command itself
-    for(j = 1; j < argc; j++) {
-      strcpy(args[i++], argv[j]);
-    }
-    j = 0;  
-    while (i < MAXARG-1) {
-      if ((len = read(0, &buf, 1)) <= 0) {
-        // CTRL+D 
-        wait(); 
+    if(argc < 2)
+    {
+        fprintf(2, "usage: xargs <cmd> ...\n");
         exit();
-      }
-      if (buf == '\n') {
-        break;
-      }
-      if (buf == ' ') {
-        if (first_blank) {
-          i++;
-          j = 0;
-          first_blank = 0;
+    }
+    while(1)
+    {
+        i = 0;
+        first_blank = 0;
+        memset(args, 0, MAXARG * 32);
+        for(j = 1; j < argc; j++)
+        {
+            strcpy(args[i++], argv[j]);
         }
-        continue;
-      }
-      args[i][j++] = buf;
-      first_blank = 1;
+        j = 0;
+        while(i < MAXARG-1)
+        {
+            if((len = read(0, &buf, 1)) <= 0)
+            {
+                wait();
+                exit();
+            }
+            if (buf == '\n')
+            {
+                break;
+            }
+            if (buf == ' ')
+            {
+                if (first_blank)
+                {
+                    i++;
+                    j = 0;
+                    first_blank = 0;
+                }
+                continue;
+            }
+            args[i][j++] = buf;
+            first_blank = 1;
+        }
+        for(i = 0; i < MAXARG-1; i++)
+        {
+            p[i] = args[i];
+        }
+        p[MAXARG-1] = 0;
+        if((pid = fork()) == 0)
+        {
+            exec(argv[1], p);
+            exit();
+        }
     }
-    for (i = 0; i < MAXARG-1; i++) {
-      p[i] = args[i];
-    }
-    // the last arg must be 0
-    p[MAXARG-1] = 0;
-    // exec command
-    if ((pid = fork()) == 0) {
-      exec(argv[1], p);
-      exit();
-    }
-  }
-  exit();
+    exit();
 }
